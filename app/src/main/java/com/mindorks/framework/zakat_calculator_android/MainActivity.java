e2837454959ca74e86a11756412a71a694f65bd2
github_username <mdrayhan9464@gmail.com>
2021-04-24 16:37:35 +0600

    button onclick

--- app/src/main/java/com/mindorks/framework/zakat_calculator_android/MainActivity.java
+++ app/src/main/java/com/mindorks/framework/zakat_calculator_android/MainActivity.java
@@ -3,12 +3,32 @@ package com.mindorks.framework.zakat_calculator_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button calculateZakatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        initObject();
+        calculateZakatButton.setOnClickListener( this );
+    }
+
+    private void initObject() {
+        calculateZakatButton = findViewById( R.id.calculate_zakat_button );
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.calculate_zakat_button:
+                Toast.makeText( this, "button clicked", Toast.LENGTH_SHORT ).show();
+                break;
+        }
     }
 }
