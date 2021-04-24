41a604912070a294b3ee65c7e9b79db605c32e60
github_username <mdrayhan9464@gmail.com>
2021-04-24 16:57:54 +0600

    nisab radiogroup

--- app/src/main/java/com/mindorks/framework/zakat_calculator_android/MainActivity.java
+++ app/src/main/java/com/mindorks/framework/zakat_calculator_android/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -30,5 +31,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText( this, "button clicked", Toast.LENGTH_SHORT ).show();
                 break;
         }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.gold:
+                if (checked)
+                    // Pirates are the best
+                    Toast.makeText( this, "Gold", Toast.LENGTH_SHORT ).show();
+                    break;
+            case R.id.silver:
+                if (checked)
+                    // Ninjas rule
+                    Toast.makeText( this, "Silver", Toast.LENGTH_SHORT ).show();
+                    break;
+        }
+
     }
 }
